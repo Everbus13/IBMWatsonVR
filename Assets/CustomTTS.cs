0d3caeaa2cbda08f1edd70d1ff4b6498dec1d898
DazedFury <jmtexas19@gmail.com>
2018-11-02 13:18:34 -0400

    refactor: Cleanup code and files

--- Assets/CustomTTS.cs
+++ Assets/CustomTTS.cs
@@ -8,13 +8,15 @@ using IBM.Watson.DeveloperCloud.Connection;
 using System;
 
 public class CustomTTS : MonoBehaviour {
-    
+
     string _testString = "no";
     private static string _username = "b5064546-7bc8-4306-aefd-12424c275da2";
     private static string _password = "B172qSzTrvro";
     private static string _serviceUrl = "https://stream.watsonplatform.net/text-to-speech/api";
+
     // Use this for initialization
-    void Start () {
+    void Start()
+    {
         Credentials credentials = new Credentials(_username, _password, _serviceUrl);
         TextToSpeech _service = new TextToSpeech(credentials);
 
